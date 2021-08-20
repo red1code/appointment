b1e225041cccce4b60af27aa84142754eebc90d9
Redouane Bekkouche <Redouane Bekkouche>
2021-08-20 15:34:19 +0100

    some changes

--- src/app/components/waiting-list/waiting-list.component.ts
+++ src/app/components/waiting-list/waiting-list.component.ts
@@ -5,7 +5,6 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Patient } from 'src/app/models/patient';
 import { Observable } from 'rxjs';
-import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-waiting-list',
@@ -14,25 +13,19 @@ import * as firebase from 'firebase';
 })
 export class WaitingListComponent implements OnInit {
 
-  newPatient!: FormGroup;
   firebaseErrorMessage: string;
-  name = new FormControl('');
-  phone = new FormControl('');
+
+  name = new FormControl('', Validators.required);
+  phone = new FormControl('', Validators.required);
 
   constructor(private authService: AuthService, private fireStore: AngularFirestore) {
     this.firebaseErrorMessage = '';
   }
 
 
-  ngOnInit(): void {
-    this.newPatient = new FormGroup({
-      'fullName': new FormControl('', Validators.required),
-      'phoneNumber': new FormControl('', Validators.required),
-    });
-  }
+  ngOnInit(): void {}
 
   submitForm() {
-    // if (this.newPatient.invalid) return;
     this.fireStore.doc('/patientsList/' + this.name.value).set({
       name: this.name.value,
       phone: this.phone.value
